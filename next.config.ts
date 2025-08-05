0f0c99e37611e6175f8f2b5d099f5582b6042846
Ansh <ansh.chauhan29@gmail.com>
2025-08-05 11:44:32 +0530

    testing reverse proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/video-editor/',
+  basePath: '/video-editor',
   assetPrefix: '/video-editor/.next',
   trailingSlash: false,
 
