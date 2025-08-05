7170055f6d0e90487108f675d6d7bf7023f98c60
Ansh <ansh.chauhan29@gmail.com>
2025-08-05 11:56:11 +0530

    testing reverse proxy

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // basePath: '/video_editor',
-  assetPrefix: '/video_editor',
-  trailingSlash: false,
+  // assetPrefix: '/video_editor',
+  // trailingSlash: false,
 
   webpack: (config, { isServer }) => {
     if (!isServer) {
