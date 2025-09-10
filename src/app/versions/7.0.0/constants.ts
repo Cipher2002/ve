e617c9cb98be2be771c3d272a6b77c67417f6725
ansh <ansh.chauhan269@gmail.com>
2025-09-10 17:53:42 +0530

    testing more than 8 layers

--- src/app/versions/7.0.0/constants.ts
+++ src/app/versions/7.0.0/constants.ts
@@ -2,7 +2,7 @@ import { Overlay, OverlayType } from "./types";
 
 // Default and maximum number of rows to display in the editor
 export const INITIAL_ROWS = 5;
-export const MAX_ROWS = 8;
+export const MAX_ROWS = 16;
 // Frames per second for video rendering
 export const FPS = 30;
 
